bb871a0618a19147947d58b6f5656b5670d7dd81
alessandro <alessandro.9403@hotmail.it>
2018-02-08 17:52:08 +0100

    correzione insertDB

--- ScriptDB/insertDB.sql
+++ ScriptDB/insertDB.sql
@@ -1,48 +1,38 @@
 INSERT INTO "Agenzia"(nome)
-  VALUES ('ESA');
-INSERT INTO "Agenzia"(nome)
-  VALUES ('NASA');
+  VALUES
+    ('ESA'),
+    ('NASA');
 
 
-INSERT INTO "Satellite"(nome,"dataInizio","dataFine")
-  VALUES ('Herschel','2009-07-10','2013-06-17');
-INSERT INTO "Satellite"(nome,"dataInizio","dataFine")
-  VALUES ('Spitzer','2003-12-18','2009-05-15');
+INSERT INTO "Satellite"(nome,"dataInizio","dataFine","nomeAgenzia")
+  VALUES
+    ('Herschel','2009-07-10','2013-06-17','ESA'),
+    ('Spitzer','2003-12-18','2009-05-15','NASA');
 
 
 INSERT INTO "Banda"(lunghezza)
-  VALUES (70);
-INSERT INTO "Banda"(lunghezza)
-  VALUES (160);
-INSERT INTO "Banda"(lunghezza)
-  VALUES (250);
-INSERT INTO "Banda"(lunghezza)
-  VALUES (350);
-INSERT INTO "Banda"(lunghezza)
-  VALUES (500);
-INSERT INTO "Banda"(lunghezza)
-  VALUES (3.6);
-INSERT INTO "Banda"(lunghezza)
-  VALUES (4.5);
-INSERT INTO "Banda"(lunghezza)
-  VALUES (5.8);
-INSERT INTO "Banda"(lunghezza)
-  VALUES (8.0);
-INSERT INTO "Banda"(lunghezza)
-  VALUES (24);
+  VALUES
+  (70),
+  (160),
+  (250),
+  (350),
+  (500),
+  (3.6),
+  (4.5),
+  (5.8),
+  (8.0),
+  (24);
 
 
 INSERT INTO "Strumento"(nome, "nomeSatellite")
-  VALUES ('Herschel-PACS','Herschel');
-INSERT INTO "Strumento"(nome, "nomeSatellite")
-  VALUES ('Herschel-SPIRE','Herschel');
-INSERT INTO "Strumento"(nome, "nomeSatellite")
-  VALUES ('Spitzer-IRAC','Spitzer');
-INSERT INTO "Strumento"(nome, "nomeSatellite")
-  VALUES ('Spitzer-MIPS','Spitzer');
+  VALUES
+    ('Herschel-PACS','Herschel'),
+    ('Herschel-SPIRE','Herschel'),
+    ('Spitzer-IRAC','Spitzer'),
+    ('Spitzer-MIPS','Spitzer');
 
 
 INSERT INTO "Utente"(nome, cognome, username, password, email, "tipoUtente")
-  VALUES ('Franco','Gialli','FrancoG94','francobasi94','FrancoG94@gmail.com','utenteRegitrato');
-INSERT INTO "Utente"(nome, cognome, username, password, email, "tipoUtente")
-  VALUES ('Marco','Rossi','MarcoR94','marcobasi94','MarcoR94@gmail.com','Amministartore');
+  VALUES
+    ('Franco','Gialli','FrancoG94','francobasi94','FrancoG94@gmail.com','utenteRegitrato'),
+    ('Marco','Rossi','MarcoR94','marcobasi94','MarcoR94@gmail.com','Amministartore');
